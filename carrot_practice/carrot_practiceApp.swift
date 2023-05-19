039c209e2f31b7d20365b2ecad8fe7de2f0b34a1
이준후 <hprize04@gmail.com>
2023-05-19 11:36:27 +0900

    Initial Commit

---
+++ carrot_practice/carrot_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  carrot_practiceApp.swift
+//  carrot_practice
+//
+//  Created by 이준후 on 2023/05/19.
+//
+
+import SwiftUI
+
+@main
+struct carrot_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
